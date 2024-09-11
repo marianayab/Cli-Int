e91bd6e36784850f7fbf23830122e227ce9cb5e1
marianayab <128246098+marianayab@users.noreply.github.com>
2024-09-11 21:50:47 +0500

    changes saved

--- src/components/Projects/data.js
+++ src/components/Projects/data.js
@@ -1,6 +1,6 @@
 const projectData = [
   {
-    id: 1,
+    id: 7,
     projectName: "RNG ( Prototype )",
     desc: "The Random Number Simulator is a streamlined tool built with React,   allows users to simulate the randomness of number selection within a specified range.",
     demo: "https://simulator-9897be.netlify.app/",
@@ -9,7 +9,7 @@ const projectData = [
   },
 
   {
-    id: 2,
+    id: 6,
     projectName: "Flight agent ( NLP )",
     desc: "Highlighting the collaboration between Dialogflow and JavaScript, showcasing how they work together to create a robust and responsive chatbot.",
     demo: "https://bot-assist-385366.netlify.app/",
@@ -18,7 +18,7 @@ const projectData = [
   },
 
   {
-    id: 3,
+    id: 5,
     projectName: "My Portfolio ( UI/UX )",
     desc: "The portfolio features an interactive user interface developed with React, backed by a solidly built Node.js and Express.js server, with MongoDB handling data management.",
     demo: "https://maria-nayab-9c44a6.netlify.app/",
@@ -35,7 +35,7 @@ const projectData = [
   },
 
   {
-    id: 5,
+    id: 3,
     projectName: "Select Cipher ( Cryptography )",
     desc: "This app is designed to demonstrate the fundamentals of cryptography, making it an educational tool as well as a functional application for securing basic communication.",
     demo: "https://crypto-cucurucho-560862.netlify.app/",
@@ -44,16 +44,16 @@ const projectData = [
   },
 
   {
-    id: 6,
+    id: 2,
     projectName: "Todo list ( Web App )",
-    desc: "A simple and intuitive To-Do app developed using JavaScript, HTML & CSS. that allows users to add, edit, check off, and delete tasks, providing a straightforward task management solution.",
+    desc: "A straightforward task management solution developed using JavaScript, HTML & CSS, that allows users to add, edit, check off, and delete tasks.",
     demo: "https://todolist678.netlify.app/",
     imgsrc: "/todo image.jpg",
     code:"https://github.com/marianayab/Todo-app",
   },
 
   {
-    id: 7,
+    id: 1,
     projectName: "Flip and Match Game ( Game Development )",
     desc: "Created a web game using HTML, JavaScript, and CSS. Test your memory and concentration as you uncover matching pairs by flipping cards.",
     demo: "https://flip-andmatchgame-e16d84.netlify.app/",
