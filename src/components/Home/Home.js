eda58521cb7e81e2b3e9e8f6858d7f8afcc198fe
marianayab <128246098+marianayab@users.noreply.github.com>
2024-11-01 17:54:04 +0500

    commit changes

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,8 +4,16 @@ import Button from "react-bootstrap/Button";
 import Lottie from "lottie-react";
 import animation from "./animation.json";
 import { Dropdown, DropdownButton } from "react-bootstrap";
-import { FaReact, FaNodeJs, FaJsSquare, FaHtml5, FaCss3Alt, FaPython, FaAws } from "react-icons/fa";
-import { SiExpress, SiMongodb, SiMysql } from 'react-icons/si';
+import {
+  FaReact,
+  FaNodeJs,
+  FaJsSquare,
+  FaHtml5,
+  FaCss3Alt,
+  FaPython,
+  FaAws,
+} from "react-icons/fa";
+import { SiExpress, SiMongodb, SiMysql } from "react-icons/si";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
 
 const Home = () => {
@@ -20,7 +28,6 @@ const Home = () => {
     { name: "Services", icon: <FaAws /> },
     { name: "MongoDB", icon: <SiMongodb /> },
     { name: "Python", icon: <FaPython /> },
-    
   ];
 
   const styles = {
@@ -28,12 +35,12 @@ const Home = () => {
       fontSize: "1.5rem",
       marginBottom: "4px",
     },
-    
+
     dropdownItem: {
       display: "flex",
       alignItems: "center",
       fontSize: "1.1rem",
-      color:"#1e3c56",
+      color: "#1e3c56",
     },
   };
 
@@ -53,50 +60,70 @@ const Home = () => {
 
             <div className="d-flex btn_div mt-4">
               <a href="https://cvlite.netlify.app/" target="_blank">
-                <Button
-                  variant="info"
-                  className="text-blue font resume hovr"> Resume </Button> </a>
+                <Button variant="info" className="text-blue font resume hovr">
+                  {" "}
+                  Resume{" "}
+                </Button>{" "}
+              </a>
 
-              <ButtonGroup className="soc-links" aria-label="Basic example">
-                <Button
-                  variant="secondary"
-                  style={{ backgroundColor: "#1e3c56" }}
+              <ButtonGroup
+                className="soc-links"
+                aria-label="Basic example"
+                style={{ backgroundColor: "#1e3c56" }}
+              >
+                <a
+                  href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox"
+                  target="_blank"
                 >
-                  <a href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox" target='_blank'>
-                    <i className="fa-brands fa-g" style={{color:'white'}} ></i>
-                  </a>
-                </Button>
-                <Button
-                  variant="secondary"
-                  style={{ backgroundColor: "#1e3c56" }}
+                  <Button
+                    className="mylnks"
+                    variant="secondary"
+                    style={{ backgroundColor: "#1e3c56" }}
+                  >
+                    <i
+                      className="fa-brands fa-g"
+                      style={{ color: "white" }}
+                    ></i>
+                  </Button>
+                </a>
+                <a
+                  href="https://www.linkedin.com/in/maria-nayab-6bb4a0278/"
+                  target="_blank"
                 >
-                  <a href="https://www.linkedin.com/in/maria-nayab-6bb4a0278/" target='_blank'> 
-                    <i className="fa-brands fa-linkedin" style={{color:'white'}} ></i>
-                  </a>
-                </Button>
+                  <Button
+                    className="mylnks lnk-style"
+                    variant="secondary"
+                    style={{ backgroundColor: "#1e3c56" }}
+                  >
+                    <i
+                      className="fa-brands fa-linkedin"
+                      style={{ color: "white" }}
+                    ></i>
+                  </Button>
+                </a>
 
-                <Button
-                  variant="secondary"
-                  style={{ backgroundColor: "#1e3c56" }}
-                >
-                  <a href="https://github.com/marianayab" target='_blank'>
-                    <i className="fa-brands fa-github" style={{color:'white'}} ></i>
-                  </a>
-                </Button>
+                <a href="https://github.com/marianayab" target="_blank">
+                  <Button
+                    className="mylnks"
+                    variant="secondary"
+                    style={{ backgroundColor: "#1e3c56" }}
+                  >
+                    <i
+                      className="fa-brands fa-github"
+                      style={{ color: "white" }}
+                    ></i>
+                  </Button>
+                </a>
               </ButtonGroup>
-
 
               <DropdownButton id="dropdown-basic-button" title="Tech Stack">
                 {skills.map((skill, index) => (
                   <Dropdown.Item key={index} style={styles.dropdownItem}>
-                    <span style={styles.skillIcon}>
-                      {skill.icon}
-                    </span>
+                    <span style={styles.skillIcon}>{skill.icon}</span>
                     {skill.name}
                   </Dropdown.Item>
                 ))}
               </DropdownButton>
-              
             </div>
           </div>
 
